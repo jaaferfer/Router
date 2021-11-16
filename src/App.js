cfc4ce70e1621d869b77d1cb148f9f6b3c585384
jaafer <jaaferfer9@gmail.com>
2021-11-16 22:49:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import {dataMovies} from './Data';
+import {useState} from 'react'
+import Filter from './components/Filter/Filter';
+import Add from './components/Add/Add';
+import MovieList from './components/MovieList/MovieList';
+import './App.css';
+import { Route, Switch } from "react-router-dom";
+import Details from './components/Details';
+
+function App() {
+const addMovie =(newMovie)=>{
+ setMovieList ( [...movieList,{...newMovie, id:movieList.length+1}]) 
+} 
+
+const [searchByName, setSearchByName] = useState('')
+
+const searchName =(name)=>{
+  setSearchByName(name)
+}
+
+
+const [movieList, setMovieList] = useState(dataMovies)
+const [rating, setRating] = useState(1)
+
+  
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path='/'>
+      <Filter searchName={searchName} setRating={setRating} />
+
+      <MovieList
+        movieList={movieList.filter(
+          (el) =>
+            el.title
+              .toUpperCase()
+              .includes(searchByName.toUpperCase().trim()) &&
+            el.rating >= rating
+        )}
+      />
+      <Add addMovie={addMovie} />
+      </Route>
+      <Route path='/details/:id' render={(props)=><Details {...props} movieList={movieList}/>} />
+      </Switch>
+
+    </div>
+   
+
+  );
+}
+
+export default App;
